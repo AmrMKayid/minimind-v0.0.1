64b9fcd105c3e8389673917e1800729fa4da7a86
Amr Kayid <amrmkayid@gmail.com>
2023-10-18 22:27:20 -0400

    Add packages

---
+++ minimind/cortex.py
@@ -0,0 +1,18 @@
+import gymnasium as gym
+
+
+def main() -> None:
+    env = gym.make("CartPole-v1", render_mode="human")
+
+    observation, info = env.reset(seed=42)
+    for _ in range(1000):
+        action = env.action_space.sample()
+        observation, reward, terminated, truncated, info = env.step(action)
+
+        if terminated or truncated:
+            observation, info = env.reset()
+    env.close()
+
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,32 @@
+import os
+from importlib import util as import_util
+
+from setuptools import find_packages, setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Get the long description from the README.mf file
+with open(os.path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+
+version = "0.0.0"
+
+setup(
+    name="minimind 🧠",
+    version=version,
+    description="Reinforcement Learning and Brain Research in JAX 🧠",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AmrMKayid/minimind",
+    author="Amr Kayid",
+    author_email="amrmkayid@gmail.com",
+    packages=find_packages(),
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
+)
